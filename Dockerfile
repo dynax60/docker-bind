d3840c45db74248f917a75e8b98f8260d9547eff
null <d.davydov@autoconnex.ru>
2020-02-03 10:42:17 +0000

    Add entrypoint script.
    This script needed to fix permissions if you want to mount your local files /etc/passwd and /etc/group to container.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM alpine:latest
 
 RUN apk --update add bind bind-tools bind-plugins && rm -rf /var/cache/apk/
 
+COPY ./entrypoint.sh /
+
 EXPOSE 53 53/udp
 
+ENTRYPOINT ["/entrypoint.sh"]
 CMD ["named", "-c", "/etc/bind/named.conf", "-g", "-u", "named"]
---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+/usr/bin/install -d -o root -g named -m 0750 /etc/bind 
+find /etc/bind/* -type d -exec chown root:root {} \;
+find /etc/bind -type f  -exec chown root:root {} \;
+find /etc/bind -type f  -exec chmod 644 {} \;
+find /etc/bind -type f -name rndc.key -exec chown named:named {} \;
+find /etc/bind -type f -name rndc.key -exec chmod 644 {} \;
+
+/usr/bin/install -d -o named -g named -m 0755 /var/cache/bind 
+chown -R named:named /var/cache/bind
+find /etc/bind -type d -exec chmod 755 {} \;
+find /etc/bind -type f -exec chmod 644 {} \;
+
+/usr/bin/install -d -o named -g named -m 0755 /var/log/named
+chown -R named:named /var/log/named
+find /etc/bind -type f -exec chmod 644 {} \;
+
+exec $*
